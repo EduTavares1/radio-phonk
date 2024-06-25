6c8957257290a908e772703f6488a79b9eaf14b5
Rômulo Henri <romulohenri000@gmail.com>
2024-06-24 23:30:22 -0300

    add: GifUpload

---
+++ src/components/GifUpload.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="file" @change="uploadGif" />
+    <img :src="gifUrl" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const gifUrl = ref('')
+
+    const uploadGif = (event) => {
+      const file = event.target.files[0]
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        gifUrl.value = e.target.result
+      }
+      reader.readAsDataURL(file)
+    }
+
+    return { gifUrl, uploadGif }
+  },
+}
+</script>
